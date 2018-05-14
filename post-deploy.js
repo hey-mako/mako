0b4fd1cece05503534f569c454bd047a09256f08
Brandon Him <brandonhim@live.com>
2018-05-14 00:56:12 -0700

    Update to post script

--- post-deploy.js
+++ post-deploy.js
@@ -1,50 +1,84 @@
 const request = require('request');
+const rp = require('request-promise');
 const converter = require('number-to-words');
 
+process.env.APP_REVIEW_ID = '2186735398249287';
+process.env.APP_REVIEW_SECRET = '317090cb5e6612a3c1d473df80b3dd33';
+
 const reviewAppId = process.env.APP_REVIEW_ID;
-const herokuAppName = process.env.HEROKU_APP_NAME;
+const herokuAppName = (process.env.HEROKU_APP_NAME) ? process.env.HEROKU_APP_NAME : `test-heroku-pr-${Math.floor(Math.random() * 100)}`;
 
 const fbBaseUrl = 'https://graph.facebook.com/v2.12';
-const callbackUrl = `https://${herokuAppName}.ngrok.io`;
+const callbackUrl = `https://${herokuAppName}.ngrok.io/facebook/receive`;
 
-const subOptions = {
-	uri: `${fbBaseUrl}/${reviewAppId}/subscriptions`,
-	method: 'POST',
-	qs: {
-		callback_url: callbackUrl, // eslint-disable-line camelcase
-		object: 'page',
-		fields: 'messages',
-		verify_token: 'TOKEN', // eslint-disable-line camelcase
-	},
-};
-
-request(subOptions, err => {
-	if (err) {
-		throw new Error(err);
-	}
-
-	console.log(`Webhook subscription set on ${callbackUrl}`);
-});
-
-const testUserOptions = {
+const createTestUser = {
 	uri: `${fbBaseUrl}/${reviewAppId}/accounts/test-users`,
 	method: 'POST',
 	qs: {
-		name: normalizeBuldName(herokuAppName),
+		permissions: 'manage_pages,publish_actions,publish_pages',
 		access_token: `${reviewAppId}|${process.env.APP_REVIEW_SECRET}`, // eslint-disable-line camelcase
 	},
 };
 
-request(testUserOptions, (err, data) => {
-	if (err) {
-		throw new Error(err);
-	}
-	const parsed = JSON.parse(data);
+rp(createTestUser)
+	.then(data => {
+		const testUser = JSON.parse(data);
 
-	process.env.TEST_USER_ID = parsed.id;
-	process.env.TEST_USER_EMAIL = parsed.email;
-	console.log(`Test user created for ${parsed.email} on ${herokuAppName}`);
-});
+		process.env.TEST_USER_ID = testUser.id;
+		process.env.TEST_USER_EMAIL = testUser.email;
+		console.log(`Test user created for ${testUser.email} on ${herokuAppName}`);
+
+		const createTestPage = {
+			url: `${fbBaseUrl}/${testUser.id}/accounts`,
+			body: {
+				category_enum: 'PERSONAL_BLOG',
+				name: 'Mako AI Test',
+				about: 'Test page for review app',
+				cover_photo: {
+					url: 'https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg'
+				},
+				picture: 'https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg'
+			},
+			json: true,
+			qs: {
+				access_token: testUser.access_token
+			},
+		};
+		return rp.post(createTestPage)
+			.then(page => {
+				return rp({
+					url: `${fbBaseUrl}/me/accounts`,
+					qs: {
+						access_token: testUser.access_token
+					}
+				}).then(body => {
+					const parsed = JSON.parse(body);
+					const pageToken = parsed.data[0].access_token;
+					return rp.post({
+						url: `${fbBaseUrl}/${page.id}/subscribed_apps`,
+						qs: {
+							access_token: pageToken
+						}
+					})
+				});
+			});
+		
+	})
+	.then(pageToken => {
+		const webhookSubscription = {
+					uri: `${fbBaseUrl}/${reviewAppId}/subscriptions`,
+					qs: {
+						callback_url: callbackUrl, // eslint-disable-line camelcase
+						object: 'page',
+						fields: 'messages',
+						verify_token: 'TOKEN', // eslint-disable-line camelcase
+					},
+				};
+			
+				return rp.post(webhookSubscription);
+			})
+	})
+
 
 function normalizeBuldName(name) {
 	const prString = name.match(/\d+/g)[0];
