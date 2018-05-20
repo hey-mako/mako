ed1150ccaa8bf3eae4f0edd4242e11f53ab9b45f
brh55 <brandonhim@live.com>
2018-05-20 10:31:58 -0400

    Fix lint issues

--- profile.js
+++ profile.js
@@ -1,5 +1,7 @@
 module.exports = controller => {
-    controller.api.nlp.enable(process.env.WIT_AI_TOKEN);
-    controller.api.messenger_profile.greeting('The best way to find your next apartment');
-    controller.api.messenger_profile.get_started('GET_STARTED');
+	controller.api.nlp.enable(process.env.WIT_AI_TOKEN);
+	controller.api.messenger_profile.greeting(
+		'The best way to find your next apartment'
+	);
+	controller.api.messenger_profile.get_started('GET_STARTED');
 };
